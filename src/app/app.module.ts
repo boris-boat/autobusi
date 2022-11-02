7c41f042f04f41db9a9ed2ec19a46cb3b3b6f172
Boris Milosavljevic <boris.milosavljevic31@gmail.com>
2022-11-02 12:59:21 +0100

    radi filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AutobusComponent } from './autobus/autobus.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import * as moment from 'moment';
 @NgModule({
   declarations: [
     AppComponent,
-    AutobusComponent,
     HomepageComponent
   ],
   imports: [
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { clippingParents } from '@popperjs/core';
+import * as moment from 'moment';
 import autobusi from "../timetable"
 
 @Component({
@@ -7,15 +9,31 @@ import autobusi from "../timetable"
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
-
+  autobusiZaDisplay: any = []
+  filter: string = ""
   constructor() { }
 
   ngOnInit(): void {
-    console.log(autobusi);
+    this.autobusiZaDisplay = autobusi
   }
-  test: string = "testiramo input"
-  printaj(item: any) {
-    console.log(item);
+  setFilter(filter: string):void {
+    for (let i in this.autobusiZaDisplay) {
+      this.autobusiZaDisplay[i].focusDan = this.autobusiZaDisplay[i][filter]
+    }
+  }
+  next30Mins():void{
+    let datum = `${new Date().getMonth()+1}.${new Date().getDate()}.${new Date().getFullYear()}`
+    let current = moment(new Date())  
+    console.log(current,moment(`${datum} 13:00`))
+    console.log(moment.duration(current.diff(moment(`${datum} 13:30`))).asMinutes())    
+    for (let i in this.autobusiZaDisplay) {
+      let temp = this.autobusiZaDisplay[i].focusDan.filter((vreme:string) => {
+        let filter = moment.duration(current.diff(moment(`${datum} ${vreme}`))).asMinutes()
+       if (filter < 0 && filter > -30) return vreme
+      else return null
+      })
+        this.autobusiZaDisplay[i].focusDan = temp
+        }
+  }
 
-  }
 }
